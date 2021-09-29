aab583aec48eec9f6d9937502025de9e89a9dc53
김다빈 <mokdb17@gmail.com>
2021-09-29 23:14:16 +0900

    식품 선택 유횽 등록 레이아웃 추가

---
+++ app/src/main/java/com/example/caneat/Register_disease_activity.java
@@ -0,0 +1,15 @@
+package com.example.caneat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register_disease_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_disease);
+    }
+}
+
---
+++ app/src/main/java/com/example/caneat/Register_religion_activity.java
@@ -0,0 +1,14 @@
+package com.example.caneat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register_religion_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_religion);
+    }
+}
+
