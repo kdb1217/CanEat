9e0293087a7e58d988af5a37712c4905a1c8cbda
김다빈 <mokdb17@gmail.com>
2021-10-01 20:41:55 +0900

    카카오 로그인 추가

--- app/src/main/java/com/example/caneat/IntroActivity.java
+++ app/src/main/java/com/example/caneat/IntroActivity.java
@@ -11,23 +11,6 @@ public class IntroActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro); // xml파일과 자바파일 연결
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                Intent intent = new Intent (getApplicationContext(), SelectActivity.class);
-                startActivity(intent); //인트로 실행후 MainActivity 실행
-                finish();
-            }
-        },1500); //1.5초후 인트로 실행
-
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        finish();
-        //멈추는 함수
     }
 
 }
