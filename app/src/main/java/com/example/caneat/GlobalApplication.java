4eebc14a47896c120a8af00e57e96820c032e137
김다빈 <mokdb17@gmail.com>
2021-10-06 00:58:40 +0900

    로그인 거의 완성

---
+++ app/src/main/java/com/example/caneat/GlobalApplication.java
@@ -0,0 +1,17 @@
+package com.example.caneat;
+
+import android.app.Application;
+import com.kakao.sdk.common.KakaoSdk;
+
+public class GlobalApplication extends Application {
+    private static GlobalApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        // 네이티브 앱 키로 초기화
+        KakaoSdk.init(this, "{KAKAO_bec3f6cab9fb0a2ba54b6b866c36ba73}");
+    }
+}
